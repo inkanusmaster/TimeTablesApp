fe83c6d4aa4e1a7726e31fd015a0aae10b62e1dc
inkanusmaster <furymistrz@gmail.com>
2019-05-11 20:13:48 +0200

    Connect ListView with Seekbar and apply changes on list while bar changes

---
+++ app/src/main/java/com/example/section5_timetables/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.section5_timetables;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.SeekBar;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ArrayList<Integer> numberArrayList = new ArrayList<>();
+    ArrayAdapter<Integer> arrayAdapter;
+    ListView tableListView;
+
+    public void fillTableListView(int value) {
+        numberArrayList.clear();
+        arrayAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, numberArrayList);
+        tableListView = findViewById(R.id.tableListView);
+        tableListView.setAdapter(arrayAdapter);
+        for (int i = 1; i <= 10; i++) {
+            numberArrayList.add(i * value);
+        }
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        SeekBar tableSeekBar = findViewById(R.id.tableSeekBar);
+        tableSeekBar.setMax(20);
+        tableSeekBar.setProgress(10);
+        fillTableListView(tableSeekBar.getProgress());
+
+        tableSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+                Log.i("Seek bar value:", Integer.toString(i));
+                if (i==0) {
+                    Toast.makeText(MainActivity.this, "NOT FOR ZERO!!", Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(MainActivity.this, "Seek bar value: " + i, Toast.LENGTH_SHORT).show();
+                    fillTableListView(i);
+                }
+            }
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+            }
+        });
+    }
+}
